9c21fd8bc817b7ed1ac4ea1f7cea38353b5b87b9
User <Willspri@gmail.com>
2025-03-10 11:29:02 +1300

    update

--- timeLocation.js
+++ timeLocation.js
@@ -4,17 +4,44 @@
 
 //function getLocations(time)
 
-const time = prompt("Please enter time: ")
+//const time = prompt("Please enter time: ")
 
-function displayLocation() {
+//function displayLocation() {
 
 //while Intl.DateTimeFormat = time
-let locations = 
+//let locations = 
 
-console.log("It is currently ${time} in these locations")
+//console.log("It is currently ${time} in these locations")
 
-}
+//}
 
 //let time = prompt("Please enter time: ")
 
-displayLocation(time)
+//displayLocation(time)
+
+function getLocations(time) {
+	const timeZones - Intl.supportedValuesOf("timezone");
+	let matchingLocations = [];
+
+	timeZones.forEach(tz => {
+		let formattedTime = new Intl.DateTimeFormat('en-US', {
+			timeZone: tz,
+			hour: '2-digit',
+			minute: '2-digit',
+			hour12: false
+		}).format(new Date());
+		
+		if (formattedTime === time) {
+			matchingLocations.push(tz);
+		}
+	});
+
+	return matchingLocations;
+}
+
+const time = prompt("Enter time: (HH:MM, 24h)");
+
+const locations = getLocations(time):
+
+
+
