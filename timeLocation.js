12fcc65c9769bd61a4ba2611f73392e2d55d91be
User <Willspri@gmail.com>
2025-03-10 12:06:14 +1300

    fixed

--- timeLocation.js
+++ timeLocation.js
@@ -1,39 +1,52 @@
-const readline = require('readline');
+const readline = require("readline");
+
+// add all
+const timeZones = [
+    "UTC", "America/New_York", "America/Chicago", "America/Denver", "America/Los_Angeles",
+    "Europe/London", "Europe/Paris", "Europe/Berlin", "Asia/Tokyo", "Asia/Shanghai",
+    "Asia/Kolkata", "Australia/Sydney", "Pacific/Honolulu"
+];
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
+//return areas that are within hour rather than specific minute
 function getLocations(time) {
-	const timeZones = Intl.supportedValuesOf("timezone");
-	let matchingLocations = [];
+    let matchingLocations = [];
 
-	timeZones.forEach(tz => {
-		let formattedTime = new Intl.DateTimeFormat('en-US', {
-			timeZone: tz,
-			hour: '2-digit',
-			minute: '2-digit',
-			hour12: false
-		}).format(new Date());
+    timeZones.forEach(tz => {
+        let formattedTime = new Intl.DateTimeFormat("en-US", {
+            timeZone: tz,
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false
+        }).format(new Date());
 
-		if (formattedTime === time) {
-			matchingLocations.push(tz);
-		}
-	});
+        if (formattedTime === time) {
+            matchingLocations.push(tz);
+        }
+    });
 
-	return matchingLocations;
+    return matchingLocations;
 }
 
-//const time = prompt("Enter time: (HH:MM, 24h)");
-rl.question("Enter time: (HH:MM, 24h)", (time)=> {
-	const locations = getLocations(time);
+rl.question("Enter time (HH:MM, 24h): ", (time) => {
+    if (!/^\d{2}:\d{2}$/.test(time)) {
+        console.log("Invalid format. Please use HH:MM in 24-hour format.");
+        rl.close();
+        return;
+    }
 
-	if (locations.length > 0) {
-		console('Locations where time = ${time} ')
-		console.log(locations.join(", "));
-	} else {
-		console.log("No locations found");
-	}
-	rl.close();
+    const locations = getLocations(time);
+
+    if (locations.length > 0) {
+        console.log(`It is currently ${time} in these locations:`);
+        console.log(locations.join(", "));
+    } else {
+        console.log(`No locations found for ${time}.`);
+    }
+
+    rl.close();
 });
