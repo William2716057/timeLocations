b7499934637c92528668f74f6b3c978fca0f6014
User <Willspri@gmail.com>
2025-03-10 11:17:12 +1300

    started

---
+++ timeLocation.js
@@ -0,0 +1,20 @@
+
+//enter time
+//where time Intl.DateTimeFormat
+
+//function getLocations(time)
+
+const time = prompt("Please enter time: ")
+
+function displayLocation() {
+
+//while Intl.DateTimeFormat = time
+let locations = 
+
+console.log("It is currently ${time} in these locations")
+
+}
+
+//let time = prompt("Please enter time: ")
+
+displayLocation(time)
